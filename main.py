cfc5ad3dcefdc2fcf15b47779adb10e43eaa5689
tmorelato <tiagomorelatocursosonline@gmail.com>
2023-07-30 11:58:16 -0300

    Versão 30-07-2023

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ import telebot
 CHAVE_API = "6348037969:AAGKSPAbKFgBxPn1RnFV2CIcLAufu8wHL2w"
 bot= telebot.TeleBot(CHAVE_API)
 def verificar(mensagem):
-        return false
+        return true
 
 @bot.message_handler(func=verificar)
 def responder(mensagem):
     texto = """
        Escolha uma das opções para Continuar (Clique No item):
-       /1 - Suporte Avacorp - 1
+       /1 - Suporte Avacorp 
        /2 - Suporte Avacorp
        /3 - Suporte InfraEstrutura Matão
        Responder Qualque outra opção não vai funcionar , clique em uma das opções"""
